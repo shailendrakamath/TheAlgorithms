c9172d698f3bd4f8a690b0acc7a54936c3617f7c
Quang Nguyen <Quang.NguyenVuNhat@nashtechglobal.com>
2018-11-28 16:17:44 +0700

    TheAlgorithms

---
+++ src/main/java/ciphers/compressclient.java
@@ -0,0 +1,14 @@
+package ciphers;
+
+import Compression.src.*;
+
+public class compressclient {
+
+	public static void main(String[] args) {
+
+		HEncoder h= new HEncoder("aaaabbbcccccccccccdddd");
+		System.out.println(h.compress("aabccd"));
+		System.out.println(h.decompress("101011000111"));
+	}
+
+}
